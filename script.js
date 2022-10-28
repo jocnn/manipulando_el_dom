62f782846a60fd129f3bf97e32a1991f59c4042c
jocnn <ijann_@outlook.com>
2022-10-28 20:36:45 -0300

    se creó una función y se cambió a función flecha

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const btnAdd = document.querySelector("[data-form-btn-add]")
 console.log(btnAdd)
 
-btnAdd.addEventListener("click", function () {
-	console.log("Crear tarea")
+const createTask = (e) => {
+	e.preventDefault()
 	const inputTask = document.querySelector("[data-form-input-task]")
 	console.log(inputTask.value)
-})
+}
+
+btnAdd.addEventListener("click", createTask)
