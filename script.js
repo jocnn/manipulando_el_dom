6a203a99e327d4891c9d2f45f0a588f49fd51e3e
jocnn <ijann_@outlook.com>
2022-10-29 12:47:44 -0300

    se agregó html a la lista usando atributo de datos

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ const createTask = (e) => {
 	const inputTask = document.querySelector("[data-form-input-task]")
 	const valueInput = inputTask.value
 	inputTask.value = ""
-	console.log(valueInput)
+	const liTask = document.querySelector("[data-task]")
+	const content = `
+		<div>
+			<i class="far fa-check-square icon"></i>
+			<span class="task">${valueInput}</span>
+		</div>
+		<i class="fas fa-trash-alt trashIcon icon"></i>
+	`
+	liTask.innerHTML = content
+	//console.log(valueInput)
+	//console.log(liTask)
 }
 
 btnAdd.addEventListener("click", createTask)
