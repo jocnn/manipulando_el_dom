179017adf037822944343a56225455f3a2c62bf6
jocnn <ijann_@outlook.com>
2022-10-29 16:24:55 -0300

    se creó lista usando js, se agrego evento click

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
 const btnAdd = document.querySelector("[data-form-btn-add]")
-console.log(btnAdd)
 
 const createTask = (e) => {
 	e.preventDefault()
@@ -10,16 +9,54 @@ const createTask = (e) => {
 	const list = document.querySelector("[data-list]")
 	const task = document.createElement("li")
 	task.classList.add("card")
-	const content = `
-		<div>
-			<i class="far fa-check-square icon"></i>
-			<span class="task">${valueInput}</span>
-		</div>
-		<i class="fas fa-trash-alt trashIcon icon"></i>
-	`
-	task.innerHTML = content
 
+	// se creó el div
+	const taskContent = document.createElement("div")
+	// se agrego el icon checklist al div
+	taskContent.appendChild(checkComplete())
+
+	// se creó el span
+	const titleTask = document.createElement("span")
+	// se agrego la clase task al span
+	titleTask.classList.add("task")
+	// se agregó el contenido del input al span
+	titleTask.innerText = valueInput
+
+	// se agregó el span al div
+	taskContent.appendChild(titleTask)
+
+	// se agregó el icono de basura al div
+	taskContent.appendChild(trashDelete())
+
+	// se agregó la tarea al elemento li
+	task.appendChild(taskContent)
+
+	// se agregó el elemento li a la lista (ul) data-list
 	list.appendChild(task)
 }
 
 btnAdd.addEventListener("click", createTask)
+
+const checkComplete = () => {
+	const i = document.createElement("i")
+	i.classList.add("far", "fa-check-square", "icon")
+	// se agregó el evento click a la etiqueta i
+	i.addEventListener("click", completeTask)
+
+	return i
+}
+
+const completeTask = (e) => {
+	const element = e.target
+	// se agregó iconos usando toggle para marcar y desmarcar la tarea
+	element.classList.toggle("fas")
+	element.classList.toggle("completeIcon")
+	element.classList.toggle("far")
+}
+
+const trashDelete = () => {
+	const i = document.createElement("i")
+	i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon")
+
+	return i
+}
